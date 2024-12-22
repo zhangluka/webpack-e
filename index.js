9ca33ab38b24798025824ab2d29024c0b50a1c26
bobby <zhangluuka@gmail.com>
2024-12-22 16:29:35 +0800

    supply the loader md

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import Sidebar from './sidebar.js'
 import avator from './assets/avator.jpg'
 import './index.css'
-import cStyle from './styles.module.scss'
+import './styles.module.scss'
 import CreateAvatar from './createAvatar.js'
 // var root = document.getElementById("container");
 // new Content();
@@ -12,8 +12,7 @@ CreateAvatar()
 var root = document.getElementById("container");
 var img = new Image();
 img.src = avator;
-console.log(cStyle);
-img.classList.add(cStyle["avator-sass"]);
+img.classList.add("avator-sass");
 root?.append(img)
 
 new Sidebar();
---
+++ loader.md
@@ -0,0 +1,3 @@
+so, what can `loader` used for?
+
+we know that `webpack` can help us to pack our code, but only can process the code in `js` file, we can't pack some files like css, image, etc. into our bundle. so, we need to use `loader` to load these files into webpack.
